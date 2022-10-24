bbc93dba01e683fafbd600117bb7c04cbf448e28
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-24 16:00:50 -0400

    Make base grid gen function work

--- script.js
+++ script.js
@@ -1,1 +1,80 @@
-console.log('Properly linked');
+const container = document.querySelector('.container');
+
+/**
+ * Makes a grid.
+ * @param {number} size The number of squares per row / column.
+ * Note this number is squared to generate the grid.
+ */
+function makeGrid(size) {
+  let squareLength = 960 / size;
+  // goes to size^2
+  for (let square = 0; square < (size * size); square++) {
+    const div = document.createElement('div');
+    div.style.height = `${squareLength}px`;
+    div.style.width = `${squareLength}px`;
+    div.classList.add('square');
+    container.appendChild(div);
+  }
+}
+
+makeGrid(16);
+/* 
+
+- set squareLength var = to pixels per square
+- use loop to make each square
+- loop will iterate ${size} times
+- at each iteration:
+  - make a div
+  - set div:
+    - height
+    - width
+    - background color
+    - display
+  - append div to container
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+- 
+
+*/
+
+
+// function makeGrid(size) {
+//   const gridDivs = [];
+//   for (let i = 0; i < size * size; i++) {
+//       window[`box${i}`] = document.createElement('div');
+//       window[`box${i}`].width = `${960/size}px`;
+//       window[`box${i}`].height = `${960/size}px`;
+//       window[`box${i}`].display = 'inline-block';
+//       window[`box${i}`].backgroundColor = 'black';
+//       window[`box${i}`].innerHTML = '&nbsp';
+//       container.appendChild(window[`box${i}`]);
+//       gridDivs.push(window[`box${i}`]);
+//     }
+// }
+
+// container.style.height = `${5 * 100}px`;
+
+// makeGrid(16);
